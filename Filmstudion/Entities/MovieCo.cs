cc5687842aa746e0f39fbcb87fbda1f8970752e7
Tom Mårtensson <Tom.martenssson@gmail.com>
2021-06-03 22:20:48 +0200

    JwtToken implementerat fungerar dock inte riktigt som det ska ännu

--- Filmstudion/Entities/MovieCo.cs
+++ Filmstudion/Entities/MovieCo.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +10,22 @@ namespace Filmstudion.Entities
     public class MovieCo
     {
         public int id { get; set; }
+       
+        [Required]
         public string name { get; set; }
+      
+        [Required]
+        public string Password { get; set; }
+       
+        [Required]
         public string place
         {
             get; set;
         }
+        
         public int Number { get; set; }
-        public string email { get; set; }
+        
+        
         public string nameOfCo { get; set; }
     }
 }
--- Filmstudion/Startup.cs
+++ Filmstudion/Startup.cs
@@ -15,11 +15,18 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using AutoMapper;
+using Filmstudion.Entities;
+using Microsoft.AspNetCore.Identity;
+using Filmstudion.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Filmstudion
 {
     public class Startup
     {
+        private string _config;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -30,6 +37,21 @@ namespace Filmstudion
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<MovieCoModel, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            services.AddAuthentication()
+                .AddCookie()
+                .AddJwtBearer(
+                cfg => {
+                    cfg.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = Configuration["Tokens:Issuer"],
+                        ValidAudience = Configuration["Tokens:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Tokens:Key"]))
+                    };
+                    }
+                    );
             services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("Filmstudion"));
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddControllers();
@@ -49,7 +71,9 @@ namespace Filmstudion
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
+
 
             app.UseEndpoints(endpoints =>
             {
